d2e9a54b1efad1cae7deb6995df359c2d24f2fb5
szlekk <somi.laszlo.b23if@kmf.org.ua>
2025-05-11 01:12:53 +0300

    v1.0

--- config/dev.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-Config::set('default-controller', "home");
-Config::set("default_action", 'index');
-Config::set('debug', true);
-Config::set('api.version', 'v1');
-Config::set('domain', explode('.', $_SERVER['HTTP_HOST']));
-Config::set('app.name', 'CMS MVC');
-Config::set('layout.default', 'default');
--- config/email.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-Config::set('email.debug', true);
-Config::set('email.user', '');
-Config::set('email.password', '');
-Config::set('email.host', '');
-Config::set('email.from', '');
-Config::set('email.reply', '');
